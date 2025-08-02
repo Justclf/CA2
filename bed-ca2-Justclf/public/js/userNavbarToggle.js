11f4afc2c64b1fd135f5fd111d22d8e10afbe7ec
LF <103033676+Justclf@users.noreply.github.com>
2025-08-02 16:25:46 +0800

    Add files via upload

--- bed-ca2-Justclf/public/js/userNavbarToggle.js
+++ bed-ca2-Justclf/public/js/userNavbarToggle.js
@@ -3,6 +3,10 @@ document.addEventListener("DOMContentLoaded", function () {
   const registerButton = document.getElementById("registerButton");
   const profileButton = document.getElementById("profileButton");
   const logoutButton = document.getElementById("logoutButton");
+  const questButton = document.getElementById("questbutton");
+  const questProgressButton = document.getElementById("questProgressButton");
+  const reportButton = document.getElementById("reportButton");
+  const pvpButton = document.getElementById("pvpButton");
 
   // Check if token exists in local storage
   const token = localStorage.getItem("token");
@@ -10,14 +14,30 @@ document.addEventListener("DOMContentLoaded", function () {
     // Token exists, show profile button and hide login and register buttons
     loginButton.classList.add("d-none");
     registerButton.classList.add("d-none");
+    questProgressButton.classList.add("d-none");
+    questButton.classList.add("d-none");
+    reportButton.classList.add("d-none");
+    pvpButton.classList.add("d-none");
     profileButton.classList.remove("d-none");
     logoutButton.classList.remove("d-none");
+    questButton.classList.remove("d-none");
+    questProgressButton.classList.remove("d-none");
+    reportButton.classList.remove("d-none")
+    pvpButton.classList.remove("d-none")
   } else {
     // Token does not exist, show login and register buttons and hide profile and logout buttons
-    loginButton.classList.remove("d-none");
-    registerButton.classList.remove("d-none");
-    profileButton.classList.add("d-none");
-    logoutButton.classList.add("d-none");
+    profileButton.classList.remove("d-none");
+    logoutButton.classList.remove("d-none");
+    questButton.classList.remove("d-none");
+    questProgressButton.classList.remove("d-none");
+    reportButton.classList.remove("d-none")
+    pvpButton.classList.remove("d-none")
+    loginButton.classList.add("d-none");
+    registerButton.classList.add("d-none");
+    questProgressButton.classList.add("d-none");
+    questButton.classList.add("d-none");
+    reportButton.classList.add("d-none");
+    pvpButton.classList.add("d-none");
   }
 
   logoutButton.addEventListener("click", function () {
